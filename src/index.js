90a201df0bd499c9d384c8be15356f5d5f92d7fd
charles peters <charlespeters42@gmail.com>
2017-02-11 00:01:55 -0800

    Few UX tweets

--- src/index.js
+++ src/index.js
@@ -5,17 +5,20 @@ import Clipboard from 'clipboard'
 
 import './index.css'
 
-const showshow = new showdown.Converter()
-
 const Mark = props =>
   <svg width='25' height='15.38461538' viewBox='0 0 208 128'>
     <rect width='198' height='118' x='5' y='5' ry='10' stroke={props.color} strokeWidth='10' fill='none'/>
     <path fill={props.color} d='M30 98v-68h20l20 25 20-25h20v68h-20v-39l-20 25-20-25v39zM155 98l-30-33h20v-35h20v35h20z'/>
   </svg>
 
-const ClipboardButton = props => <button className='Clipboard' id='clipboard' onClick={props.onClipboard}>Copy</button>
-const Button = props => <button className='Button' onSubmit={props.onSubmit} children='Submit' />
-const WordCount = props => <span className='Input__count' children={props.number} />
+const ClipboardButton = props =>
+  <button className='Clipboard' id='clipboard' onClick={props.onClipboard} children='Copy'/>
+
+const Button = props =>
+  <button className='Button' onSubmit={props.onSubmit} children='Submit' />
+
+const WordCount = props =>
+  <span className='Input__count' children={props.number} />
 
 const Switch = ({ onChange, checked, value }) =>
   <label className='Switch' id='checkUpper'>
@@ -87,6 +90,7 @@ class App extends Component {
   }
 
   render() {
+    const showshow = new showdown.Converter()
 
     const lowlow = () => {
       if (this.state.upper) {
