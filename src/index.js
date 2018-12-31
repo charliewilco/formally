2fba32ed0f4d4492417b06c8a0c647e68e868d4d
Charles ⚡ <charlespeters42@gmail.com>
2018-12-31 00:42:01 -0800

    adding local fonts

--- src/index.js
+++ src/index.js
@@ -1,112 +1,148 @@
-import React, { Component } from 'react'
-import { render } from 'react-dom'
-import registerServiceWorker from './registerServiceWorker'
-import showdown from 'showdown'
-import Clipboard from 'clipboard'
-import 'typeface-hind'
-import './index.css'
+import React, { Component } from "react";
+import { render } from "react-dom";
+import registerServiceWorker from "./registerServiceWorker";
+import showdown from "showdown";
+import Clipboard from "clipboard";
+import "./index.css";
 
 const Mark = ({ color }) => (
-  <svg width='25' height='15.38461538' viewBox='0 0 208 128'>
-    <rect width='198' height='118' x='5' y='5' ry='10' stroke={color} strokeWidth='10' fill='none'/>
-    <path fill={color} d='M30 98v-68h20l20 25 20-25h20v68h-20v-39l-20 25-20-25v39zM155 98l-30-33h20v-35h20v35h20z'/>
+  <svg width="25" height="15.38461538" viewBox="0 0 208 128">
+    <rect
+      width="198"
+      height="118"
+      x="5"
+      y="5"
+      ry="10"
+      stroke={color}
+      strokeWidth="10"
+      fill="none"
+    />
+    <path
+      fill={color}
+      d="M30 98v-68h20l20 25 20-25h20v68h-20v-39l-20 25-20-25v39zM155 98l-30-33h20v-35h20v35h20z"
+    />
   </svg>
-)
+);
 
 const ClipboardButton = ({ onClipboard }) => (
-  <button className='Clipboard' id='clipboard' onClick={onClipboard} children='Copy'/>
-)
+  <button
+    className="Clipboard"
+    id="clipboard"
+    onClick={onClipboard}
+    children="Copy"
+  />
+);
 
 const Button = ({ onSubmit }) => (
-  <button className='Button' onSubmit={onSubmit} children='Submit' />
-)
+  <button className="Button" onSubmit={onSubmit} children="Submit" />
+);
 
-const WordCount = ({ number }) => <span className='Input__count' children={number} />
+const WordCount = ({ number }) => (
+  <span className="Input__count" children={number} />
+);
 
 const Switch = ({ onChange, checked, value }) => (
-  <label className='Switch' id='checkUpper'>
-    <input className='Switch__check' onChange={onChange} checked={checked} htmlFor='checkUpper' type='checkbox' />
-    <span className='Switch__label'>Uppercase?</span>
+  <label className="Switch" id="checkUpper">
+    <input
+      className="Switch__check"
+      onChange={onChange}
+      checked={checked}
+      htmlFor="checkUpper"
+      type="checkbox"
+    />
+    <span className="Switch__label">Uppercase?</span>
   </label>
-)
-
+);
 
 const Input = ({ onSubmit, onChange, onChecked, checked, value }) => (
-  <form className='Input' onSubmit={onSubmit}>
+  <form className="Input" onSubmit={onSubmit}>
     <div>
       <textarea
-        className='Input__area'
-        id='area'
+        className="Input__area"
+        id="area"
         value={value}
         onChange={onChange}
       />
       <WordCount number={value.length} />
     </div>
-    <div className='Input__tray'>
-      <div className='Input__tray__content'>
-        <div className='Input__tray__content'>
-          <Mark color='rgba(30, 184, 235, 0.35)' />
-          <label htmlFor='area' className='Input__label'>Convert</label>
+    <div className="Input__tray">
+      <div className="Input__tray__content">
+        <div className="Input__tray__content">
+          <Mark color="rgba(30, 184, 235, 0.35)" />
+          <label htmlFor="area" className="Input__label">
+            Convert
+          </label>
         </div>
         <Switch checked={checked} onChange={onChecked} />
       </div>
       <Button onSubmit={onSubmit} />
     </div>
   </form>
-)
+);
 
-function createMarkup (x) {
-  return { __html: x }
+function createMarkup(x) {
+  return { __html: x };
 }
 
 const Content = ({ onClipboard, content }) => (
-  <div className='Output'>
-    <div className='Output__content'>
+  <div className="Output">
+    <div className="Output__content">
       <ClipboardButton onClick={onClipboard} />
-      <div id='content' className='__markdown' dangerouslySetInnerHTML={createMarkup(content)} />
+      <div
+        id="content"
+        className="__markdown"
+        dangerouslySetInnerHTML={createMarkup(content)}
+      />
     </div>
   </div>
-)
+);
 
-const { localStorage } = window
+const { localStorage } = window;
 
 class App extends Component {
   state = {
-    value: localStorage.getItem('lowerCaseValue') || '',
+    value: localStorage.getItem("lowerCaseValue") || "",
     upper: false
-  }
+  };
 
-  _onSubmit = e => e.preventDefault()
+  _onSubmit = e => e.preventDefault();
 
-  _onChange = e => this.setState({
-      value: e.target.value
-    }, localStorage.setItem('lowerCaseValue', this.state.value))
+  _onChange = e =>
+    this.setState(
+      {
+        value: e.target.value
+      },
+      localStorage.setItem("lowerCaseValue", this.state.value)
+    );
 
-  _onSwitch = () => this.setState({ upper: !this.state.upper })
+  _onSwitch = () => this.setState({ upper: !this.state.upper });
 
-  _onClipboard = x => new Clipboard('#clipboard', {
-    text() { return x }
-  })
+  _onClipboard = x =>
+    new Clipboard("#clipboard", {
+      text() {
+        return x;
+      }
+    });
 
   _lowlow = () => {
-    const showshow = new showdown.Converter()
+    const showshow = new showdown.Converter();
 
     if (this.state.upper) {
-      return showshow.makeHtml(this.state.value.toUpperCase())
+      return showshow.makeHtml(this.state.value.toUpperCase());
     } else {
-      return showshow.makeHtml(this.state.value.toLowerCase())
+      return showshow.makeHtml(this.state.value.toLowerCase());
     }
-  }
+  };
 
-  componentDidMount () {
-    console.log(localStorage.getItem('lowerCaseValue'))
+  componentDidMount() {
+    console.log(localStorage.getItem("lowerCaseValue"));
   }
 
   render() {
-    const { value } = this.state
+    const { value } = this.state;
 
     return (
-      <div className='App'>
+      <div className="App">
         <Input
           value={value}
           onSubmit={this._onSubmit}
@@ -119,9 +155,9 @@ class App extends Component {
           onClipboard={this._onClipboard(value.toLowerCase())}
         />
       </div>
-    )
+    );
   }
 }
 
-render(<App />, document.getElementById('root'))
-registerServiceWorker()
+render(<App />, document.getElementById("root"));
+registerServiceWorker();
