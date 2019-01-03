35dd5cb31fe0c0c46f22469de1749d9ac00c06c5
Charles ⚡ <charlespeters42@gmail.com>
2019-01-03 12:45:20 -0800

    design updates

--- src/button.tsx
+++ src/button.tsx
@@ -3,7 +3,9 @@ import * as React from "react";
 const Button: React.SFC<{
   onSubmit: (e: React.SyntheticEvent) => void;
 }> = ({ onSubmit }) => (
-  <button className="Button" onSubmit={onSubmit} children="Submit" />
+  <button className="Button" onSubmit={onSubmit}>
+    Convert
+  </button>
 );
 
 export default Button;
