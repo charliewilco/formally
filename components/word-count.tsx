3e9886b8e91e531bd0fb5d9a27ad4091a9fa5b6b
Charles ⚡ <charlespeters42@gmail.com>
2020-12-05 00:55:02 -0800

    bye

--- components/word-count.tsx
+++
@@ -1,7 +0,0 @@
-import * as React from "react";
-
-const WordCount: React.FC<{ number: number }> = ({ number }) => (
-  <span className="Input__count" children={number} />
-);
-
-export default WordCount;
