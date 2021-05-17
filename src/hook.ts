16cfb67ebed68a062c9783e171a043fcd014db4e
Charlie ⚡ <4588060+charliewilco@users.noreply.github.com>
2021-05-17 01:24:55 -0700

    finding things correctly

---
+++ src/hook.ts
@@ -0,0 +1,62 @@
+import { useEffect, useReducer, Reducer } from "react";
+import { reducer, ILowLowState, Action, LowLowActions } from "./reducer";
+
+const copyContent = async (value: string) => {
+  try {
+    await navigator.clipboard.writeText(value);
+    console.log("Copied to clipboard");
+  } catch (err) {
+    console.error("Failed to copy: ", err);
+  }
+};
+const LOCAL_STORAGE_KEY = "lowerCaseValue";
+
+type HTMLParser = (value: string) => string;
+
+export const useLowLow = (parser: HTMLParser) => {
+  const [{ isUpper, value, converted }, dispatch] = useReducer<
+    Reducer<ILowLowState, Action>
+  >(reducer, {
+    isUpper: false,
+    converted: "",
+    value: "",
+  });
+  useEffect(() => {
+    if (localStorage) {
+      const item = localStorage.getItem(LOCAL_STORAGE_KEY);
+
+      if (item) {
+        dispatch({ type: LowLowActions.GET_VALUE_FROM_STORAGE, payload: item });
+      }
+    }
+  }, []);
+
+  useEffect(() => {
+    if (localStorage) {
+      localStorage.setItem(LOCAL_STORAGE_KEY, value);
+    }
+  }, [value]);
+
+  useEffect(() => {
+    const v = isUpper ? value.toUpperCase() : value.toLowerCase();
+    const html = parser(v);
+    dispatch({ type: LowLowActions.SET_CONVERTED, payload: html });
+  }, [value, isUpper]);
+
+  const onSubmit = (e: React.SyntheticEvent): void => e.preventDefault();
+  const onChange = (e: React.ChangeEvent<HTMLTextAreaElement>): void =>
+    dispatch({ type: LowLowActions.SET_VALUE, payload: e.target.value });
+  const onToggle = () => dispatch({ type: LowLowActions.SET_UPPER });
+  const onCopy = () =>
+    copyContent(isUpper ? value.toUpperCase() : value.toLowerCase());
+
+  return [
+    { isUpper, value, converted },
+    {
+      onToggle,
+      onChange,
+      onSubmit,
+      onCopy,
+    },
+  ] as const;
+};
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,88 +1,23 @@
-import { useState, useEffect, useCallback, CSSProperties } from "react";
 import Head from "next/head";
 import remark from "remark";
 import html from "remark-html";
-
-const LOCAL_STORAGE_KEY = "lowerCaseValue";
+import { useLowLow } from "../hook";
 
 const makeHTML = (value: string) => {
   const parsed = remark().use(html).processSync(value);
 
-  return parsed;
-};
-
-const usePersistedState = () => {
-  const [value, setValue] = useState<string>("");
-  useEffect(() => {
-    if (localStorage) {
-      const item = localStorage.getItem(LOCAL_STORAGE_KEY);
-
-      if (item) {
-        setValue(item);
-      }
-    }
-  }, []);
-
-  useEffect(() => {
-    if (localStorage) {
-      localStorage.setItem(LOCAL_STORAGE_KEY, value);
-    }
-  }, [value]);
-
-  return [value, setValue] as const;
-};
-
-const useLowLow = () => {
-  const [value, setValue] = usePersistedState();
-  const [isUpper, setIsUpper] = useState<boolean>(false);
-  const [converted, setConverted] = useState<string>("");
-
-  useEffect(() => {
-    const v = isUpper ? value.toUpperCase() : value.toLowerCase();
-    const html = makeHTML(v);
-    setConverted(html.toString("utf8"));
-  }, [value, isUpper]);
-
-  const onSubmit = (e: React.SyntheticEvent): void => e.preventDefault();
-  const onChange = (e: React.ChangeEvent<HTMLTextAreaElement>): void =>
-    setValue(e.target.value);
-  const onToggle = () => setIsUpper((prev) => !prev);
-  const onCopy = () =>
-    copyContent(isUpper ? value.toUpperCase() : value.toLowerCase());
-
-  return [
-    { isUpper, value, converted },
-    {
-      onToggle,
-      onChange,
-      onSubmit,
-      onCopy,
-    },
-  ] as const;
-};
-
-const copyContent = async (value: string) => {
-  try {
-    await navigator.clipboard.writeText(value);
-    console.log("Copied to clipboard");
-  } catch (err) {
-    console.error("Failed to copy: ", err);
-  }
+  return parsed.toString("utf8");
 };
 
 const createMarkup = (__html: string) => ({ __html });
 
-const inputStyle: CSSProperties = {
-  minHeight: 300,
-};
-
-const copyButtonStyle: CSSProperties = {
+const copyButtonStyle = {
   top: -20,
   right: -20,
 };
 
 const IndexPage = () => {
-  const [state, actions] = useLowLow();
+  const [{ value, isUpper, converted }, actions] = useLowLow(makeHTML);
 
   return (
     <div className="px-2">
@@ -95,25 +30,24 @@ const IndexPage = () => {
 
         <title>LowLow</title>
       </Head>
-      <header className="mb-6 py-3 container container-md mx-auto border-b-2 border-gray-700  font-black ">
+      <header className="container py-3 mx-auto mb-6 font-black border-b-2 border-gray-700 container-md ">
         <h1 className="text-2xl text-red-300">LowLow</h1>
       </header>
-      <main className="py-3 container container-md mx-auto">
+      <main className="container py-3 mx-auto container-md">
         <div className="md:grid grid-cols-2 gap-4">
           <form className="mb-6" onSubmit={actions.onSubmit}>
             <div className="relative mb-6">
               <textarea
-                className="w-full rounded-md block border border-gray-700 py-4 px-2 text-white bg-gray-900 resize-none"
+                className="h-64 block w-full px-2 py-4 text-white bg-gray-900 border border-gray-700 resize-none rounded-md"
                 id="area"
-                style={inputStyle}
-                value={state.value}
+                value={value}
                 onChange={actions.onChange}
               />
-              <span className="absolute text-xxs top-0 right-0 p-3">
-                {state.value.length}
+              <span className="absolute top-0 right-0 p-3 text-xxs">
+                {value.length}
               </span>
             </div>
-            <div className="flex justify-between items-center">
+            <div className="flex items-center justify-between">
               <div className="flex items-center w-3/4">
                 <div className="flex items-center mr-4">
                   <svg width="25" height="15.38461538" viewBox="0 0 208 128">
@@ -136,13 +70,13 @@ const IndexPage = () => {
                     Convert
                   </label>
                 </div>
-                <label className="custom-label flex">
-                  <div className="bg-white shadow w-6 h-6 p-1 flex justify-center items-center mr-2">
+                <label className="flex custom-label">
+                  <div className="flex items-center justify-center w-6 h-6 p-1 mr-2 bg-white shadow">
                     <input
                       type="checkbox"
                       className="hidden"
                       onChange={actions.onToggle}
-                      checked={state.isUpper}
+                      checked={isUpper}
                     />
 
                     <svg
@@ -155,8 +89,7 @@ const IndexPage = () => {
                         fontFamily="none"
                         fontWeight="none"
                         fontSize="none"
-                        textAnchor="none"
-                        style={{ mixBlendMode: "normal" }}>
+                        textAnchor="none">
                         <path d="M0 172V0h172v172z" />
                         <path
                           d="M145.433 37.933L64.5 118.8658 33.7337 88.0996l-10.134 10.1341L64.5 139.1341l91.067-91.067z"
@@ -170,7 +103,7 @@ const IndexPage = () => {
                 </label>
               </div>
               <button
-                className="bg-red-600 hover:bg-red-900 text-white font-black py-2 px-4 rounded"
+                className="px-4 py-2 font-black text-white bg-red-600 rounded hover:bg-red-900"
                 type="submit">
                 Convert
               </button>
@@ -178,17 +111,17 @@ const IndexPage = () => {
           </form>
 
           <div className="mb-6 ">
-            <div className="shadow-sm bg-gray-900 p-3 text-sm font-bold relative">
+            <div className="relative p-3 text-sm font-bold bg-gray-900 shadow-sm">
               <button
-                className="absolute bg-red-600 hover:bg-red-900 text-xs font-black rounded-full py-3 h-12 w-12 transition transition-transform duration-300 ease-in-out transform hover:scale-75 hover:-rotate-45"
+                className="absolute w-12 h-12 py-3 text-xs font-black bg-red-600 rounded-full hover:bg-red-900 transition transition-transform duration-300 ease-in-out transform hover:scale-75 hover:-rotate-45"
                 style={copyButtonStyle}
                 onClick={actions.onCopy}>
                 Copy
               </button>
               <div
                 id="content"
-                className="__markdown text-lg"
-                dangerouslySetInnerHTML={createMarkup(state.converted)}
+                className="text-lg __markdown"
+                dangerouslySetInnerHTML={createMarkup(converted)}
               />
             </div>
           </div>
